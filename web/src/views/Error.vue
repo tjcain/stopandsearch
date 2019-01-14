9e9e4724bbe7d921c34d3292df9a0e24f5556dad
tjcain <tomas.j.cain@googlemail.com>
2019-01-14 16:52:19 +0000

    added an error page

---
+++ web/src/views/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-6 is-offset-1">
+        <div class="content is-medium">
+          <h1>Sorry, something went wrong</h1>
+          <p>
+            This error has been logged and will be looked into, please
+            <router-link to="/">try again</router-link> or if the error
+            persists consider <router-link to="/contact">letting me know</router-link>.
+          </p>
+          
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.content {
+  min-height: 60vh;
+}
+</style>
+
